1f1133935b5b4739913fce712c19f4925c5ffc91
Atanas Radoynov <radoinov98@gmail.com>
2024-03-28 12:50:45 +0200

    Making Programming QA folder available by removing submodule cached data

---
+++ Programming-For-QA/Programming-Advanced-for-QA-November-2023/ExerciseUnitTestingStringsRegEx/TestApp.UnitTests/EmailValidatorTests.cs
@@ -0,0 +1,41 @@
+﻿using NUnit.Framework;
+
+namespace TestApp.UnitTests;
+
+public class EmailValidatorTests
+{
+    
+    [TestCase("atanasradoynov@gmail.com")]
+    [TestCase("atanas98radoynov@gmail.com")]
+    [TestCase("atanas.radoynov@gmail.com")]
+    [TestCase("atanas_radoynov@gmail.com")]
+    [TestCase("atanas%radoynov@gmail.com")]
+    [TestCase("atanas+radoynov@gmail.com")]
+    [TestCase("atanas-radoynov@gmail.com")]
+    public void Test_ValidEmails_ReturnsTrue(string email)
+    {
+        // Arrange
+
+        // Act
+        bool result = EmailValidator.IsValidEmail(email);
+
+        // Assert
+        Assert.That(result, Is.True);
+    }
+
+    
+    [TestCase("atanasradoynovgmail.com")]
+    [TestCase("atanasradoynov@gmailcom")]
+    [TestCase("atanasradoynov@gmail.c")]
+    [TestCase("atanasradoynov@gmail/com")]
+    public void Test_InvalidEmails_ReturnsFalse(string email)
+    {
+        // Arrange
+
+        // Act
+        bool result = EmailValidator.IsValidEmail(email);
+        
+        // Assert
+        Assert.That(result, Is.False);
+    }
+}
